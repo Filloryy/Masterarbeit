2bc0dd96a44f91fa3c27a4e27261fae78ceb3d40
Filloryy <itunsur@gmail.com>
2025-04-30 13:50:18 +0200

    Update newertry.py
    added 3 Node graph

--- newertry.py
+++ newertry.py
@@ -97,7 +97,7 @@ record_env = TransformedEnv(
 )
 record_env.transform[0].init_stats(num_iter=1000, reduce_dim=0, cat_dim=0)
 
-class actor(torch.nn.Module):
+class single_node_actor(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -119,6 +119,36 @@ class actor(torch.nn.Module):
             loc, scale = self.propagation_model(batch.x, batch.edge_index)  
         else:
             loc, scale = self.propagation_model(data.x, data.edge_index)
+            loc = loc.t().squeeze(-1)
+            scale = scale.t().squeeze(-1)
+        return loc, scale
+
+class torso_left_right_actor(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.propagation_model = Sequential("x, edge_index, batch", [
+            (Linear(11, 64), "x -> x"),
+            (GraphConv(64, 64), "x, edge_index -> x"),
+            nn.Tanh(),
+            (GraphConv(64, 64), "x, edge_index -> x"),
+            nn.Tanh(),
+            (Linear(64, 16), "x -> x"),
+            nn.Tanh(),
+            (global_mean_pool, "x, batch -> x"),
+            NormalParamExtractor(),
+        ])
+
+
+    #this is an example forward function, tried with different but somewhat similar functions    
+        #propagation model does not take a list so we unpack the list in a batch as explained in torch_geometric
+    def forward(self, data):
+        if isinstance(data, list):
+            batch = Batch.from_data_list(data)
+            loc, scale = self.propagation_model(batch.x, batch.edge_index, batch.batch)  
+        else:
+            batch_vec = torch.zeros(data.x.size(0), dtype=torch.long, device=data.x.device)
+            loc, scale = self.propagation_model(data.x, data.edge_index, batch_vec)
             loc = loc.t().squeeze(-1)
             scale = scale.t().squeeze(-1)
         return loc, scale
